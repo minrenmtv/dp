8eaeb4f465971e856d55f6a06d7de634285a1660
Min Ren <minrenmtv@gmail.com>
2013-10-03 18:45:07 -0400

    adding tornado example

---
+++ data_structure/test.py
@@ -0,0 +1,40 @@
+import unittest
+from dataStructure import Stack
+
+class TestStack(unittest.TestCase):
+
+	def setUp(self):
+		pass
+		
+	def test_empty(self):
+		s = Stack()
+		self.assertTrue(s.isEmpty())
+
+	def test_push(self):
+		s = Stack()
+		s.push('item')
+		self.assertEqual(s.items, ['item'])
+
+	def test_pop(self):
+		s = Stack()
+		s.push('item')
+		self.assertEqual(s.pop(), 'item')
+		self.assertTrue(s.isEmpty())
+
+	def test_peek(self):
+		s = Stack()
+		s.push('item')
+		self.assertEqual(s.peek(), 'item')
+		self.assertFalse(s.isEmpty())
+		self.assertEqual(s.items, ['item'])
+
+	def test_size(self):
+		s = Stack()
+		s.push('item')
+		self.assertEqual(s.size(), 1)
+
+
+
+
+if __name__ == "__main__":
+	unittest.main()
